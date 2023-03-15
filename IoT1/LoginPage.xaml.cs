a39922935a196b833f1cc04d60b7ee630bcce9ad
Sabari Krishna <Sabarikrishnaork@gmail.com>
2023-03-14 21:54:35 -0400

    Login, Registration and Location pages added.(TODO Location page)

--- IoT1/LoginPage.xaml.cs
+++ IoT1/LoginPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,59 @@ namespace IoT1
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+
+            // For Windows auth -
+            // @"Data Source=(MachineName)\InstanceName);Initial Catalog = (DBName); Integrated Security = True;"
+            // For SQL Server auth -
+            // @"Data Source=(MachineName)\InstanceName);Initial Catalog = (DBName); UserID = Username; Password = (Password);"
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localdb)\Local; Database = LoginDB;Integrated Security = True;");
+
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM  tblUser WHERE  Username = @Username AND Password =@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    MessageBox.Show("Login Successful");
+                    this.NavigationService.Navigate(null);
+                }
+
+                else
+                {
+                    MessageBox.Show("Invalid Username/Password");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterPage registerPage = new RegisterPage();
+            this.NavigationService.Navigate(registerPage);
+
         }
     }
 }
